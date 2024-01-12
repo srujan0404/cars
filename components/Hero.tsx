b73e7b221c85827c77e3f320eb347f3c007dec55
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2024-01-12 23:53:13 +0530

    changes in hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Find, Book your car now!!
         </h1>
 
         <p className="hero__subtitle">
